9744d4618f7a632c3aa44f10bb2105b23b7005ec
Ben <bhans23@gmail.com>
2020-02-14 12:00:52 -0700

    Removed loggin function

--- client/src/components/shared/Tracker.js
+++ client/src/components/shared/Tracker.js
@@ -3,31 +3,12 @@ import { Link } from "react-router-dom";
 import MoodForm from "../forms/MoodForm";
 import { AuthContext } from "../../providers/AuthContext";
 
-
 const Tracker = () => {
-  const auth = useContext(AuthContext);
-  const {
-    data: { user }
-  } = auth;
-
-  const loggedIn = () => {
-    if (user) {
-      return <MoodForm />;
-    } else {
-      return (
-        <ul>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-          <li>
-            <Link to="/register">Register</Link>
-          </li>
-        </ul>
-      );
-    }
-  };
-
-  return <div>{loggedIn()}</div>;
+  return (
+    <>
+      <MoodForm />
+    </>
+  );
 };
 
 export default Tracker;
